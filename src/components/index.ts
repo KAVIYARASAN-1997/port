0b9b7a81ab87f170b0b3bb7dda7f27a1677212ce
KAVIYARASAN (Official) <kaviyarasan1997ceo@gmail.com>
2023-02-02 01:21:16 +0530

    Create index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,24 @@
+import TopNav from './TopNav';
+import SocialLinks from './SocialLinks';
+import { ScrollIndicatorD, ScrollIndicatorM } from './ScrollIndicator';
+import Rasing from './Rasing';
+import { ProjectDeckD, ProjectDeckM } from './ProjectDeck';
+import { ProjectDetailD, ProjectDetailM } from './ProjectDetail';
+import ProjectDetailImage from './ProjectDetailImage';
+import LineIcon from './LineIcon';
+import InitialLoading from './InitialLoading';
+
+export {
+	TopNav,
+	SocialLinks,
+	ScrollIndicatorD,
+	ScrollIndicatorM,
+	Rasing,
+	ProjectDeckD,
+	ProjectDeckM,
+	ProjectDetailD,
+	ProjectDetailM,
+	ProjectDetailImage,
+	LineIcon,
+	InitialLoading,
+};
